600ec6f0dbb2f523bc79933fd6581980a54c41fe
mashtalir-sahsa <mashtalir_sasha@ukr.net>
2019-05-16 18:53:57 +0300

    0.0.1

---
+++ app/js/common.js
@@ -0,0 +1,46 @@
+$(function() {
+
+	// Скролинг по якорям
+	$('.anchor').bind("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top-106 // отступ от меню
+		}, 500);
+	e.preventDefault();
+	});
+
+	// Клик по гамбургеру на моб версии
+	$('.mob-mnu__humb').click(function() {
+		$('.mob-mnu-list').toggleClass('show');
+	});
+	$('.mob-mnu__li').click(function() {
+		$('.mob-mnu-list').removeClass('show');
+	});
+
+	// Отправка формы
+	$('form').submit(function() {
+		var data = $(this).serialize();
+		var goalId = $(this).find('input[ name="goal"]').val();
+		data += '&ajax-request=true';
+		$.ajax({
+			type: 'POST',
+			url: 'mail.php',
+			dataType: 'json',
+			data: data,
+			success: (function() {
+				$.fancybox.close();
+				$.fancybox.open('<div class="thn"><h3>Заявка отправлена!</h3><p>С Вами свяжутся в ближайшее время.</p></div>');
+				//gtag('event','submit',{'event_category':'submit','event_action':goalId});
+				//fbq('track', 'Lead');
+			})()
+		});
+		return false;
+	});
+
+	// Инит фансибокса
+	$('.fancybox').fancybox({
+		margin: 0,
+		padding: 0
+	});
+
+});
